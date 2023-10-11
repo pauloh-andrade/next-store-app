d3b7a7c1e4321361eb1d31769199281d57c2495c
Paulo <pauloh.andrade@outlook.com.br>
2023-10-10 22:55:00 -0300

    feat: added home image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
+import Image from "next/image"
 import Link from "next/link"
 
 const Home = () => {
   return (
     <>
       <h1> Home </h1>
+      <Image src="/store.svg" width={100} height={100} alt="Store icon"/>
       <ul>
         <li><Link href="/produtos/tenis">Tenis</Link></li>
         <li><Link href="/produtos/blusas">Blusas</Link></li>
