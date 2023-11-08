b271680947e1ced67ce253b7d482f9dab2442ed4
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-07 22:17:21 -0300

    feat: added login

--- src/app/api/base/users/route.jsx
+++ src/app/api/base/users/route.jsx
@@ -14,7 +14,7 @@ export async function POST(request, { params }) {
 
   try {
     for (let i = 0; i < users.usuarios.length; i++) {
-      const userFile = user.usuarios[i]
+      const userFile = users.usuarios[i]
 
       if (userFile.email == user.email && userFile.senha == user.senha) {
         return NextResponse.json({ "status": true })
@@ -23,8 +23,7 @@ export async function POST(request, { params }) {
   } catch (err) {
     console.log(err)
   }
-
-  return NextResponse.json({ "status": true })
+  return NextResponse.json({ "status": false })
 }
 
 
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,41 +1,56 @@
 "use client"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 const Login = () => {
-    const [user, setUser] = useState({ email: "", senha: ""})
+    const router = useRouter();
+
+    const [messageStatus, setStatusMessage] = useState("")
+    const [user, setUser] = useState({ email: "", senha: "" })
     
     const handleChange = (e) => {
         const {name, value} = e.target
         setUser({...user, [name]: value})   
     }
 
-    const handleSubmit = async () => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         try {
-            const res = await fetch("http://loalhost:3000/api/base/base-users/", {
+            const res = await fetch("http://localhost:3000/api/base/users/", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify(user)
             })
-
-            if (Response.ok) {
+            setStatusMessage("dasda")
+            if (res.ok) {
                 let status = await res.json()
-                if (status) {
-                    console.log("Usuário validado com sucesso")
-                } else {
-                    console.log("falha no login")
+
+                if (status.status == true) { 
+                    setStatusMessage("Login realizado com sucesso")
+                    setTimeout(() => {
+                        router.push("/")
+                    }, 5000)
                 }
+                else { 
+                    setStatusMessage("Usuário ou senha incorretos")
+                    setTimeout(() => {
+                        setMessage("")
+                        setUser({ email: "", senha: "" })
+                    }, 5000) 
+                }       
             }
         } catch (err) {
-
+            console.log(err)
         }
     }
 
     return (
         <>
+            <h1>Informçãoes do login</h1>
+            <h2>{messageStatus}</h2>
             <form onSubmit={handleSubmit}>
                 <fieldset>
                     <legend> Login </legend>
